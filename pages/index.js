4671571157520944866e74aadf9e4fd02ea60a88
t-mario-y-windows-pc-2 <t.mario.yoshi@gmail.com>
2019-07-24 19:02:34 +0900

    learn getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Layout from '../components/MyLayout.js';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
-
-const Blog = () => (
+const Index = props => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="hello, next.js" />
-      <PostLink id="learn-nextjs" title="let's learn" />
-      <PostLink id="deploy-nexjs" title="how to deploy"/>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 );
 
-export default Blog;
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,15 +1,24 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout.js';
+import fetch from 'isomorphic-unfetch';
 
-const Page = () => {
-  const router = useRouter();
-
+const Page = props => {
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
+      <h1>{props.show.name}</h1>
+      <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+      <img src={props.show.image.medium} />
     </Layout>
   );
+};
+
+Page.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
 };
 
 export default Page;
